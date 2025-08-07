adc84e8055a2f0861e265528baa2feab32e5b99f
Jakub Pentoś <jakub.pentos@tfbank.pl>
2025-08-07 12:28:09 +0200

    Dodano except to be visible do wszystkich lokatorow

--- first_test.py
+++ first_test.py
@@ -1,14 +1,20 @@
-from playwright.sync_api import sync_playwright
+from math import e
+from playwright.sync_api import sync_playwright, expect
 
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=False)
     page = browser.new_page()
     page.goto('https://www.decathlon.pl/')
+    expect(page.get_by_role("button", name="Zaakceptuj wszystko")).to_be_visible()
     page.get_by_role("button", name="Zaakceptuj wszystko").click()
+    expect(page.get_by_role("button", name="Otwórz boczną nawigację strony")).to_be_visible()
     page.get_by_role("button", name="Otwórz boczną nawigację strony").click()
+    expect(page.get_by_role("button", name="Wszystkie sporty")).to_be_visible()
     page.get_by_role("button", name="Wszystkie sporty").click()
+    expect(page.locator("#highlight-0")).to_be_visible()
     page.locator("#highlight-0").click()
+    expect(page.locator("#cat-30")).to_be_visible()
     page.locator("#cat-30").click()
+    expect(page.locator('text=Rowery górskie')).to_be_visible()
     page.click('text=Rowery górskie');
-    page.click('text=Pokazuj tylko produkty dostępne w Twoim sklepie');
     page.pause();
