5f1f819c1f7516893557cb7c2a487d9b4d170f54
Jakub Pentoś <jakub.pentos@tfbank.pl>
2025-08-11 11:56:00 +0200

    program sczytuje teraz marki, nazwy, ceny i wagi wszystkich rowerow.

--- first_test.py
+++ first_test.py
@@ -1,4 +1,5 @@
 from playwright.sync_api import sync_playwright, expect
+import time
 
 with sync_playwright() as p:
 
@@ -13,18 +14,24 @@ with sync_playwright() as p:
     page.get_by_role("button", name="Wszystkie sporty").click()
     page.locator("#highlight-0").click()
     page.locator("#cat-30").click()
+    
     test = page.locator(".sublevel a span").filter(has_text="Rowery górskie")
     test.click()
 
+    
     page.wait_for_load_state('load')
     decathlonSeler = page.locator("css=.filter-element--checkbox").filter(has=page.locator("css=#DecathlonPartner"))
     decathlonSeler.click()
     expect(decathlonSeler.locator("css=#DecathlonPartner")).to_be_checked()
     page.wait_for_load_state('load')
 
+    time.sleep(2)
+
     expect(page.locator("#list-sort-select")).to_be_visible()
     page.locator("#list-sort-select").select_option(value = '2')
     page.wait_for_load_state('load')
+    
+    time.sleep(2)
     
 
     product_links = page.locator("a.dpb-product-link")
@@ -35,14 +42,24 @@ with sync_playwright() as p:
 
         brand = link.locator("strong").inner_text().strip()
         model = link.locator("span").inner_text().strip()
-
         price_locator = link.locator("css=+ div.price-wrapper span.vtmn-mr-1").first
         price = price_locator.inner_text().replace('\xa0', ' ').strip()
 
+        link.click()
+
+        try:
+            weight = page.locator("h3:has-text('Waga') >> xpath=following-sibling::p").nth(0).inner_text()
+            weight = " ".join(weight.split())
+        except:
+            weight = "N/A"
+
+        page.go_back()
+        
         rows.append({
             "marka": brand,
             "model": model,
-            "cena": price
+            "cena": price,
+            "waga": weight
         })
 
     print(rows)
